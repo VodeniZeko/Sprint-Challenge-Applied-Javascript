07b4a74b2c1e1e70823c6d36a2f89df0773d4b8c
VodeniZeko <salexy.es@gmail.com>
2020-01-17 09:35:16 -0800

    add header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,32 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+  //creating elements
+  const header = document.createElement("div");
+  const headerSpan = document.createElement("span");
+  const headerText = document.createElement("h1");
+  const headerSpanLast = document.createElement("span");
+
+  //adding classes
+  header.classList.add("header");
+  headerSpan.classList.add("date");
+  headerText.classList.add("h1");
+  headerSpanLast.classList.add("temp");
+
+  //adding content
+  headerSpan.textContent = "MARCH 28, 2019";
+  headerText.textContent = "Lambda Times";
+  headerSpanLast.textContent = "98 \u00B0";
+
+  //appending elements
+  header.append(headerSpan);
+  header.append(headerText);
+  header.append(headerSpanLast);
+
+  return header;
+}
+
+const headerContainer = document.querySelector(".header-container");
+
+headerContainer.append(Header());
