7c0c5e3d41e5a9e8210da8a236fcdc73a85d4e1e
VodeniZeko <salexy.es@gmail.com>
2020-01-17 09:44:24 -0800

    add topics

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,21 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+function topicsCard(arr) {
+  const tab = document.createElement("div");
+  tab.classList.add("tab");
+  tab.textContent = `topic here: ${arr}`;
+
+  return tab;
+}
+
+const topics = document.querySelector(".topics");
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics").then(res => {
+  let top = res.data.topics;
+  console.log(top);
+  top.map(el => {
+    topics.append(topicsCard(el));
+  });
+});
